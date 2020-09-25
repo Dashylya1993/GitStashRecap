7c070fc864803c979d7486672af66c3e8ce8bd1c
*dashylya1993* <*dashylya1993@gmail.com*>
2020-09-24 20:04:42 -0400

    git stash pop added

--- src/com/stash/TestStash.java
+++ src/com/stash/TestStash.java
@@ -4,6 +4,8 @@ public class TestStash {
 	public static void main(String[] args) {
 		System.out.println("New modification of our code");
 		System.out.println("Hello World");
+		
+		System.out.println("git stash pop");
 	}
 
 }
